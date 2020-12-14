f9682139a99a76203f968cc4ae3d662195d8f829
unknown <atanasova.dari@gmail.com>
2020-12-14 12:52:15 +0200

    Sphere intersection function

--- Ray Tracer/main.cpp
+++ Ray Tracer/main.cpp
@@ -3,8 +3,21 @@
 #include "ray.h"
 
 #include <iostream>
+#include<fstream>
+
+bool hit_sphere(const point3& center, double radius, ray& ray) {
+    vec3 ac = ray.origin() - center; //A-C
+    auto a = dot(ray.direction(), ray.direction()); //b^2
+    auto b = 2 * dot(ray.direction(), ac); //2b(A-C)
+    auto c = dot(ac, ac) - radius * radius; //(A-C)^2-r^2
+    auto discriminant = b * b - 4 * a * c;
+    return discriminant > 0;
+}
 
 color ray_color(ray r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
@@ -31,8 +44,9 @@ int main() {
   
 
     // Render
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::ofstream file("image.ppm", std::ios::out);
+    //if(!file){}
+    file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
@@ -41,10 +55,11 @@ int main() {
             auto v = double(j) / (image_height - 1);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
             color pixel_color = ray_color(r);
-            write_color(std::cout, pixel_color);
+            write_color(file, pixel_color);
 
         }
     }
+    file.close();
     std::cerr << "\nDone.\n";
     system("pause");
 }
