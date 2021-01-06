2e85f40ffd45caa3809ecbe7e7fe5744d0f6e8da
unknown <atanasova.dari@gmail.com>
2021-01-06 12:42:09 +0200

    Made an abstract class to see if an object is hittable

---
+++ Ray Tracer/hittable.h
@@ -0,0 +1,18 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+struct hit_record {
+	point3 p;
+	vec3 normal;
+	double t;
+};
+
+class hittable {
+	public:
+		virtual bool hit(ray& ray, double t_min, double t_max, hit_record& rec) const = 0;
+};
+
+#endif // !HITTABLE_H
+
