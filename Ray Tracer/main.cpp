f07d4605ac4d84f3fd0cfbaa4a0f9498bf5f8684
unknown <atanasova.dari@gmail.com>
2021-01-06 23:44:33 +0200

    Added hittable objects

--- Ray Tracer/main.cpp
+++ Ray Tracer/main.cpp
@@ -1,36 +1,21 @@
-#include "vec3.h"
+#include "utility_functions.h"
+
 #include "color.h"
-#include "ray.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 #include<fstream>
 
-double hit_sphere(const point3& center, double radius, ray& ray) {
-    vec3 ac = ray.origin() - center; //A-C
 
-    //dot product(A, A) = (vector length)^2
-
-    auto a = ray.direction().length_squared(); // b^2 //dot(ray.direction(), ray.direction())
-    auto half_b = dot(ray.direction(), ac); //b(A-C)
-    auto c = ac.length_squared() - radius * radius; //(A-C)^2-r^2 //dot(ac, ac)
-    auto discriminant = half_b * half_b - a * c;
-
-    if (discriminant > 0) {
-        return (-half_b - sqrt(discriminant)) / a;
+color ray_color(ray r, hittable_list& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
-    else {
-        return -1;
-    }
-}
 
-color ray_color(ray r) {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
-    }
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -41,6 +26,12 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    //World
+    
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     //Camera
 
@@ -65,7 +56,7 @@ int main() {
             auto u = double(i) / (image_width - 1);
             auto v = double(j) / (image_height - 1);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(file, pixel_color);
 
         }
