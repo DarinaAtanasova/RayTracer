d059b2d18d94622fa2f358ff7d9438e5b822dac7
unknown <atanasova.dari@gmail.com>
2021-01-16 16:45:55 +0200

    Random double function

--- Ray Tracer/utility_functions.h
+++ Ray Tracer/utility_functions.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Common Headers
 #include "ray.h"
@@ -18,9 +19,25 @@ using std::sqrt;
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.14159265358979323846;
 
+
 //Utility functions
+
 inline double degrees_to_radians(double degrees) {
 	return degrees * pi / 180;
+}
+
+inline double random_double() {
+	//Random double in [0, 1)
+	static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+	static std::mt19937 generator;
+	return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+	//Random double in [min, max]
+	static std::uniform_real_distribution<double> distribution(min, max);
+	static std::mt19937 generator;
+	return distribution(generator);
 }
 
 #endif // !UTILITY_FUNCTIONS_H
