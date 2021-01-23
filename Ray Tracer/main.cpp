2ba532b647a894b2b8df27571d8d68fa50717224
unknown <atanasova.dari@gmail.com>
2021-01-23 18:37:24 +0200

    Separated camera

--- Ray Tracer/main.cpp
+++ Ray Tracer/main.cpp
@@ -3,11 +3,13 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
 #include<fstream>
 
 
+// Returns the color of the background
 color ray_color(ray r, hittable& world, int depth) {
     hit_record rec;
 
@@ -34,6 +36,7 @@ int main() {
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int max_depth = 50;
+    const int samples_per_pixel = 100;
 
     //World
     
@@ -42,16 +45,7 @@ int main() {
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     //Camera
-
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
-  
+    camera camera;
 
     // Render
     std::ofstream file("image.ppm", std::ios::out);
@@ -63,12 +57,13 @@ int main() {
         for (int i = 0; i < image_width; ++i) {
             auto u = double(i) / (image_width - 1);
             auto v = double(j) / (image_height - 1);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            ray r = camera.get_ray(u, v);
             color pixel_color = ray_color(r, world, max_depth);
-            write_color(file, pixel_color);
 
+            write_color(file, pixel_color, samples_per_pixel);
         }
     }
+
     file.close();
     std::cerr << "\nDone.\n";
     system("pause");
