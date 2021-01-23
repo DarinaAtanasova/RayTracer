a65aba03d77cb4b0de477555f127a7156d76293f
unknown <atanasova.dari@gmail.com>
2021-01-23 23:15:26 +0200

    Added lambertian(diffuse) material class

--- Ray Tracer/material.h
+++ Ray Tracer/material.h
@@ -12,5 +12,26 @@ class material {
 		)const = 0;
 };
 
+class lambertian : public material {
+	public:
+		color albedo; //the reflecting power of a material
+	public:
+		lambertian(const color& a) : albedo(a) {}
+
+		virtual bool scatter(
+			const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+		)const override {
+			auto scatter_direction = rec.normal + random_unit_vector();
+
+			if (scatter_direction.near_zero()) {
+				scatter_direction = rec.normal;
+			}
+
+			scattered = ray(rec.p, scatter_direction);
+			attenuation = albedo;
+			return true;
+		}
+};
+
 #endif // !MATERIAL_H
 
