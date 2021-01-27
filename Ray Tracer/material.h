913df7d159c8b7ccc959901a6f33c87f484412f0
unknown <atanasova.dari@gmail.com>
2021-01-27 11:29:58 +0200

    Metal material with reflection function

--- Ray Tracer/material.h
+++ Ray Tracer/material.h
@@ -8,7 +8,7 @@ struct hit_record;
 class material {
 	public:
 		virtual bool scatter(
-			const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+			ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
 		)const = 0;
 };
 
@@ -19,7 +19,7 @@ class lambertian : public material {
 		lambertian(const color& a) : albedo(a) {}
 
 		virtual bool scatter(
-			const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+			ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
 		)const override {
 			auto scatter_direction = rec.normal + random_unit_vector();
 
@@ -30,6 +30,22 @@ class lambertian : public material {
 			scattered = ray(rec.p, scatter_direction);
 			attenuation = albedo;
 			return true;
+		}
+};
+
+class metal : public material {
+	public:
+		color albedo;
+
+	public:
+		metal(const color& a) : albedo(a) {}
+		virtual bool scatter(
+			ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+		)const override {
+			vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+			scattered = ray(rec.p, reflected);
+			attenuation = albedo;
+			return (dot(scattered.direction(), rec.normal) > 0);
 		}
 };
 
