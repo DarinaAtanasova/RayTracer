c7f551e715b96c3d6d5491bff4d629328d506648
unknown <atanasova.dari@gmail.com>
2021-01-29 17:40:59 +0200

    Generating pixels with multiple samples

--- Ray Tracer/main.cpp
+++ Ray Tracer/main.cpp
@@ -50,9 +50,7 @@ int main() {
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    //auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_center = make_shared<dielectric>(1.5);
-
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left = make_shared<dielectric>(1.5);
     auto material_right = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
     
@@ -60,6 +58,7 @@ int main() {
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 
@@ -74,11 +73,14 @@ int main() {
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
-            ray r = camera.get_ray(u, v);
-            color pixel_color = ray_color(r, world, max_depth);
+            color pixel_color(0, 0, 0);
 
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = camera.get_ray(u, v);
+                pixel_color += ray_color(r, world, max_depth);
+            }
             write_color(file, pixel_color, samples_per_pixel);
         }
     }
