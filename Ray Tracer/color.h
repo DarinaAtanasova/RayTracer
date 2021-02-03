39de68b3c820ded189925794404cd9ee812f5dcf
unknown <atanasova.dari@gmail.com>
2021-02-03 14:31:38 +0200

    Deleted a comment

--- Ray Tracer/color.h
+++ Ray Tracer/color.h
@@ -17,10 +17,6 @@ void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
     b = sqrt(scale * b);
 
     // Write the translated [0,255] value of each color component.
-    /*out << static_cast<int>(255.999 * r) << ' '
-        << static_cast<int>(255.999 * g) << ' '
-        << static_cast<int>(255.999 * b) << '\n';*/
-
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
